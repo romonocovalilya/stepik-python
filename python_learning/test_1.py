479c84c647ce3c52179164686aaba80f66c2ab1a
romonocovalilya <___github@example.com>
2026-05-24 00:50:02 +0700

    Добавлены решения задач итогового экзамена

---
+++ python_learning/test_1.py
@@ -0,0 +1,35 @@
+print("Итоговый экзамен по первому модулю")
+print()
+print("Решение задания №1")
+print()
+symbol = "*"
+b = " "
+print(str*17)
+print(str, b*13, str)
+print(str, b*13, str)
+print(str*17)
+print()
+print("Решение задания №2")
+print()
+a = int(input())
+b = int(input())
+
+print("Квадрат суммы", a, "и", b, "равен", (a+b)**2)
+print("Сумма квадратов", a, "и", b, "равна", a**2+b**2)
+print()
+print("Решение задания №3")
+print()
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a**b+c**d)
+print()
+print("Решение задания №4")
+print()
+num = int(input())
+num2 = num*10+num
+num3 = num*100+num2
+
+print(num+num2+num3)
